34b7409540ced8c8c299073864b3fd124dcef864
Thomas Junk <thomas.junk@intevation.de>
2024-02-15 16:10:56 +0100

    Revert "fix: use proxy to route auth requests to keycloak"
    This reverts commit 6d8ff2c67897655e2aaa22866bd466c5b59756ab.

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -9,56 +9,56 @@
 -->
 
 <script lang="ts">
-  import { onMount } from "svelte";
-  import Keycloak from "keycloak-js";
-  import { Heading, Sidebar, SidebarWrapper, SidebarGroup, SidebarItem } from "flowbite-svelte";
+	import { onMount } from 'svelte';
+	import Keycloak from 'keycloak-js';
+	import { Heading, Sidebar, SidebarWrapper, SidebarGroup, SidebarItem } from 'flowbite-svelte';
 
-  let keycloak;
-  let token;
-  let firstName;
-  let lastName;
+	let keycloak;
+	let token;
+	let firstName;
+	let lastName;
 
-  onMount(async () => {
-    keycloak = new Keycloak({
-      url: "/auth/",
-      realm: "isduba",
-      clientId: "auth"
-    });
-    const authenticated = await keycloak
-      .init({
-        checkLoginIframe: false
-        //onLoad: 'login-required'
-      })
-      .then(async (response) => {
-        token = keycloak.token;
-        const profile = await keycloak.loadUserProfile();
-        console.log("Retrieved user profile:", profile);
-        firstName = profile.firstName;
-        lastName = profile.lastName;
-      })
-      .catch((error) => {
-        console.log("error", error);
-      });
-  });
+	onMount(async () => {
+		keycloak = new Keycloak({
+			url: 'http://localhost:8080',
+			realm: 'isduba',
+			clientId: 'auth'
+		});
+		const authenticated = await keycloak
+			.init({
+				checkLoginIframe: false
+				//onLoad: 'login-required'
+			})
+			.then(async (response) => {
+				token = keycloak.token;
+				const profile = await keycloak.loadUserProfile();
+				console.log('Retrieved user profile:', profile);
+				firstName = profile.firstName;
+				lastName = profile.lastName;
+			})
+			.catch((error) => {
+				console.log('error', error);
+			});
+	});
 
-  async function logout() {
-    keycloak.logout();
-  }
+	async function logout() {
+		keycloak.logout();
+	}
 
-  function login() {
-    keycloak.login();
-  }
+	function login() {
+		keycloak.login();
+	}
 </script>
 
 <Sidebar class="bg-primary-700 h-screen p-2">
-  <SidebarWrapper class="bg-primary-700">
-    <Heading class="mb-6 text-white">ISDuBA</Heading>
-    <SidebarGroup class="bg-primary-700">
-      {#if token}
-        <SidebarItem on:click={logout} label="Logout ({firstName} {lastName})"></SidebarItem>
-      {:else}
-        <SidebarItem on:click={login} label="Login"></SidebarItem>
-      {/if}
-    </SidebarGroup>
-  </SidebarWrapper>
+	<SidebarWrapper class="bg-primary-700">
+		<Heading class="mb-6 text-white">ISDuBA</Heading>
+		<SidebarGroup class="bg-primary-700">
+			{#if token}
+				<SidebarItem on:click={logout} label="Logout ({firstName} {lastName})"></SidebarItem>
+			{:else}
+				<SidebarItem on:click={login} label="Login"></SidebarItem>
+			{/if}
+		</SidebarGroup>
+	</SidebarWrapper>
 </Sidebar>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,21 +6,12 @@
 // SPDX-FileCopyrightText: 2024 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
 // Software-Engineering: 2024 Intevation GmbH <https://intevation.de>
 
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vitest/config";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/auth/": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/auth\//, "")
-      }
-    }
-  },
-  plugins: [sveltekit()],
-  test: {
-    include: ["src/**/*.{test,spec}.{js,ts}"]
-  }
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 });
