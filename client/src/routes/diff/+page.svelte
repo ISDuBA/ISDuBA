2184cce718670de0200b59080abd02997393e384
Christoph Klassen <christoph.klassen@intevation.de>
2024-02-19 09:32:25 +0000

    fix: Added missing licenses

--- client/src/routes/diff/+page.svelte
+++ client/src/routes/diff/+page.svelte
@@ -1,3 +1,13 @@
+<!--
+ This file is Free Software under the MIT License
+ without warranty, see README.md and LICENSES/MIT.txt for details.
+
+ SPDX-License-Identifier: MIT
+
+ SPDX-FileCopyrightText: 2024 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+ Software-Engineering: 2024 Intevation GmbH <https://intevation.de>
+-->
+
 <script lang="ts">
   import { onMount } from "svelte";
   import Diff from "$lib/Diff.svelte";
