88c27da0088a738a50f287dfed4189feb9fe63d0
JanHoefelmeyer <hoefelmeyer.jan@gmail.com>
2024-02-22 08:21:45 +0100

    Improve go and java setup script

--- docs/scripts/installgojava.sh
+++ docs/scripts/installgojava.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+apt-get update
+
 apt install -y openjdk-17-jre-headless unzip
 
 wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
@@ -10,3 +12,5 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 rm -rf go1.22.0.linux-amd64.tar.gz
 
 echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
+
+export PATH=$PATH:/usr/local/go/bin
