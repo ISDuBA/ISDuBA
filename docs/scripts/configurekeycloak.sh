733939c22fbf5469f1bba766f2ddd53146ab10c1
JanHoefelmeyer <hoefelmeyer.jan@gmail.com>
2024-02-22 11:56:14 +0100

    Add a sed-based script to alter keycloak config

---
+++ docs/scripts/configurekeycloak.sh
@@ -0,0 +1,16 @@
+!/usr/bin/env bash
+set -e
+
+DBURLB=#db-url=jdbc:postgresql://localhost/keycloak
+
+DBURLA=db-url=jdbc:postgresql://localhost/keycloak
+
+sed -i s,#db=postgres,db=postgres,g /opt/keycloak/conf/keycloak.conf
+
+sed -i s,#db-username=keycloak,db-username=keycloak,g /opt/keycloak/conf/keycloak.conf
+
+sed -i s,#db-password=password,db-password=keycloak,g /opt/keycloak/conf/keycloak.conf
+
+sed -i s,$DBURLB,$DBURLA,g /opt/keycloak/conf/keycloak.conf
+
+sed -i s,#hostname=myhostname,#hostname=isduba,g /opt/keycloak/conf/keycloak.conf
