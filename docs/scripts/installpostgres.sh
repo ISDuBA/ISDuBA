c61b9e70ee5eb5ab702cf530d4307b55f4310ca4
JanHoefelmeyer <hoefelmeyer.jan@gmail.com>
2024-02-22 13:17:53 +0100

    Add -y to skip confirmation

--- docs/scripts/installpostgres.sh
+++ docs/scripts/installpostgres.sh
@@ -5,4 +5,4 @@ apt install vim gnupg2 -y
 curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc| gpg --trust-model always --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
 sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' 
 apt update
-apt install postgresql-16
+apt install postgresql-16 -y
