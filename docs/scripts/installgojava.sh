a671e38c18bdd3d8ace231caa55789fd71eeffb7
JanHoefelmeyer <hoefelmeyer.jan@gmail.com>
2024-02-22 10:25:27 +0100

    Fix: Remove still writing to /etc/profile

--- docs/scripts/installgojava.sh
+++ docs/scripts/installgojava.sh
@@ -7,10 +7,8 @@ apt install -y openjdk-17-jre-headless
 
 wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
 
-rm  /usr/local/go -r && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+rm  /usr/local/go -rf && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 
 rm go1.22.0.linux-amd64.tar.gz
-
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
 
 ln -snf /usr/local/go/bin/go /usr/local/bin/go
