089e7bee607f2b283c4656f561a5d9e517c42ecb
Thomas Junk <thomas.junk@intevation.de>
2024-02-26 14:18:46 +0100

    fix: Add missing file

---
+++ client/src/lib/table/defaults.ts
@@ -0,0 +1,12 @@
+// This file is Free Software under the MIT License
+// without warranty, see README.md and LICENSES/MIT.txt for details.
+//
+// SPDX-License-Identifier: MIT
+//
+// SPDX-FileCopyrightText: 2024 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+//  Software-Engineering: 2024 Intevation GmbH <https://intevation.de>
+
+const tdClass = "whitespace-nowrap py-2";
+const tablePadding = "";
+
+export { tdClass, tablePadding };
