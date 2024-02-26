b3e673d226cc8c2f488f486226c0825e08e661bf
JanHoefelmeyer <hoefelmeyer.jan@gmail.com>
2024-02-26 10:39:41 +0100

    More manual node installation

--- docs/scripts/installplaywright.sh
+++ docs/scripts/installplaywright.sh
@@ -10,11 +10,13 @@
 
 set -e # to exit if a command in the script fails
 
-echo
+# install node
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install nodejs -y
 
-curl -sL https://deb.nodesource.com/setup_20.x | bash - # install node
-
-apt-get install nodejs -y # install node
 
 cd ../../client # change directory
 
