03f2441832e8141bebf9bd183fe94e5185c341f2
JanHoefelmeyer <107021473+JanHoefelmeyer@users.noreply.github.com>
2024-02-28 10:46:13 +0100

    Update configurepostgres.sh
    ' -> "

--- docs/scripts/configurepostgres.sh
+++ docs/scripts/configurepostgres.sh
@@ -19,6 +19,6 @@ apt-get install sudo                  # Need sudo to become postgres
 sudo -u postgres psql -c "CREATE USER keycloak WITH PASSWORD 'keycloak';"
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 sudo -u postgres createdb -O keycloak -E 'UTF-8' keycloak
-sudo -u postgres sed -i 's/$LAB/$LAA/g' /etc/postgresql/16/main/postgresql.conf
+sudo -u postgres sed -i "s/$LAB/$LAA/g" /etc/postgresql/16/main/postgresql.conf
 sudo -u postgres echo "host    all             all             192.168.56.1/32         scram-sha-256" >>/etc/postgresql/16/main/pg_hba.conf
 sudo -u postgres echo "host    all             all             127.0.0.1/32            scram-sha-256" >>/etc/postgresql/16/main/pg_hba.conf
