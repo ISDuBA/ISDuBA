4623ce4c9e5d35caba27e7276e41312e8bdc60a6
Thomas Junk <thomas.junk@intevation.de>
2024-03-14 08:44:25 +0100

    feat: red border around not working component on /home

---
+++ client/src/lib/Home/NewEvents.svelte
@@ -0,0 +1,38 @@
+<!--
+ This file is Free Software under the MIT License
+ without warranty, see README.md and LICENSES/MIT.txt for details.
+
+ SPDX-License-Identifier: Apache-2.0
+
+ SPDX-FileCopyrightText: 2024 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+ Software-Engineering: 2024 Intevation GmbH <https://intevation.de>
+-->
+
+<script lang="ts">
+  import {
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+    TableSearch,
+    Pagination
+  } from "flowbite-svelte";
+  import { tdClass, tablePadding } from "$lib/table/defaults";
+</script>
+
+<div class="border border-red-500">
+  <Table>
+    <TableHead>
+      <TableHeadCell padding={tablePadding}>Description</TableHeadCell>
+      <TableHeadCell padding={tablePadding}>Advisory</TableHeadCell>
+    </TableHead>
+    <TableBody>
+      <TableBodyRow>
+        <TableBodyCell {tdClass}>Comment added</TableBodyCell>
+        <TableBodyCell {tdClass}>Sick PSIRT SCA-2022-0032</TableBodyCell>
+      </TableBodyRow>
+    </TableBody>
+  </Table>
+</div>
