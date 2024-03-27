af30b4bc93816c0e1447f430a5c689196633699d
JanHoefelmeyer <Jan Hfelmeyer jhoefelmeyer@intevation.de>
2024-03-27 22:25:27 +0100

    Add migrate.sh to automate database migration, add start_isduba.sh to start backend and frontend

---
+++ docs/scripts/migrate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This file is Free Software under the Apache-2.0 License
+# without warranty, see README.md and LICENSES/Apache-2.0.txt for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# SPDX-FileCopyrightText: 2024 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+# Software-Engineering: 2024 Intevation GmbH <https://intevation.de>
+
+set -e # to exit if a command in the script fails
+
+# migrate
+ISDUBA_DB_MIGRATE=true ./../../bin/isdubad -c ../../isdubad.toml &
+
+touch isduba.log # to ensure file exists for grep
+until grep -q -F "Starting web server" isduba.log
+do
+  sleep 1
+done
+
+mv isduba.log isduba_migrate.log
+
+kill $!
---
+++ docs/scripts/start_isduba.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This file is Free Software under the Apache-2.0 License
+# without warranty, see README.md and LICENSES/Apache-2.0.txt for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# SPDX-FileCopyrightText: 2024 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+# Software-Engineering: 2024 Intevation GmbH <https://intevation.de>
+
+set -e # to exit if a command in the script fails
+
+./../../bin/isdubad -c ../../isdubad.toml &
+
+cd ./../../client/
+
+npm run dev &
