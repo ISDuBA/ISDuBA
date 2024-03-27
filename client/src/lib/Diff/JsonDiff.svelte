c67860292e06ebb3a2c4634eaefb1b6155776198
Christoph Klassen <christoph.klassen@intevation.de>
2024-03-27 08:06:10 +0000

    Make diff operations foldable

--- client/src/lib/Diff/JsonDiff.svelte
+++ client/src/lib/Diff/JsonDiff.svelte
@@ -9,7 +9,7 @@
 -->
 
 <script lang="ts">
-  import { Label } from "flowbite-svelte";
+  import { Accordion, AccordionItem, Label } from "flowbite-svelte";
   import DiffEntry from "./DiffEntry.svelte";
   import type { JsonDiffResult } from "./JsonDiff";
 
@@ -44,40 +44,44 @@
 <div>
   <Label class="text-lg">Compare Version {diff.docA} with Version {diff.docB}</Label>
   <span>{diff.result.length} changes</span>
-  {#each groupedResults as result}
-    {#if result.changes.length > 0}
-      <div>
-        {#if result.op === "add"}
-          <div class="bg-green-800 py-1 ps-2 text-white">
-            <i class="bx bx-plus text-lg"></i>
-            <span>Added ({result.changes.length})</span>
+  <Accordion flush multiple>
+    {#each groupedResults as result}
+      {#if result.changes.length > 0}
+        <AccordionItem open>
+          <div slot="header" class="pl-2">
+            {#if result.op === "add"}
+              <div class="flex items-center gap-2">
+                <i class="bx bx-plus text-lg"></i>
+                <span>Added ({result.changes.length})</span>
+              </div>
+            {:else if result.op === "remove"}
+              <div class="flex items-center gap-2">
+                <i class="bx bx-x text-lg"></i>
+                <span>Removed ({result.changes.length})</span>
+              </div>
+            {:else}
+              <div class="flex items-center gap-2">
+                <i class="bx bx-pencil text-lg"></i>
+                <span>Edited ({result.changes.length})</span>
+              </div>
+            {/if}
           </div>
-        {:else if result.op === "remove"}
-          <div class="bg-red-800 py-1 ps-2 text-white">
-            <i class="bx bx-x text-lg"></i>
-            <span>Removed ({result.changes.length})</span>
-          </div>
-        {:else}
-          <div class="bg-gray-800 py-1 ps-2 text-white">
-            <i class="bx bx-pencil text-lg"></i>
-            <span>Edited ({result.changes.length})</span>
-          </div>
-        {/if}
-      </div>
-      {#each result.changes as change}
-        <div class={getBodyClass(change.op)}>
-          <div class="mb-1">
-            <b>
-              <code>
-                {change.path}
-              </code>
-            </b>
-          </div>
-          {#if change.value}
-            <DiffEntry content={change.value} operation={change.op}></DiffEntry>
-          {/if}
-        </div>
-      {/each}
-    {/if}
-  {/each}
+          {#each result.changes as change}
+            <div class={getBodyClass(change.op)}>
+              <div class="mb-1">
+                <b>
+                  <code>
+                    {change.path}
+                  </code>
+                </b>
+              </div>
+              {#if change.value}
+                <DiffEntry content={change.value} operation={change.op}></DiffEntry>
+              {/if}
+            </div>
+          {/each}
+        </AccordionItem>
+      {/if}
+    {/each}
+  </Accordion>
 </div>
