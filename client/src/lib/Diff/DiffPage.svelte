06bc1f001c90fc9610cbf9b72e13c69f45233ca4
Christoph Klassen <christoph.klassen@intevation.de>
2024-04-08 19:29:30 +0000

    feat: Add option to show Json diff in Compare area

--- client/src/lib/Diff/DiffPage.svelte
+++ client/src/lib/Diff/DiffPage.svelte
@@ -10,27 +10,61 @@
 
 <script lang="ts">
   import { onMount } from "svelte";
-  import { appStore } from "$lib/store";
   import Diff from "$lib/Diff/Diff.svelte";
   import SectionHeader from "$lib/SectionHeader.svelte";
   import { request } from "$lib/utils";
-  import ErrorMessage from "$lib/Messages/ErrorMessage.svelte";
+  import { Button, Label, Select, TabItem, Tabs } from "flowbite-svelte";
+  import JsonDiff from "./JsonDiff.svelte";
 
-  let diff: string;
-  let error: string;
-  onMount(async () => {
-    if ($appStore.app.keycloak.authenticated) {
-      error = "";
-      const response = await request("advisory.diff", "GET");
-      if (response.ok) {
-        diff = response.content;
-      } else if (response.error) {
-        error = response.error;
+  let documents = [];
+  $: selectionOfDocuments = documents.map((doc) => {
+    return {
+      name: `${doc.tracking_id} - Version ${doc.version}`,
+      value: {
+        id: doc.id,
+        version: doc.version
       }
+    };
+  });
+  let diffDocuments: any;
+  let docA: any;
+  let docB: any;
+
+  const compare = () => {
+    if (docA && docB) {
+      diffDocuments = {
+        docA: docB,
+        docB: docA
+      };
+    }
+  };
+
+  onMount(async () => {
+    const documentURL = encodeURI(`/api/documents?limit=20&columns=id version tracking_id`);
+    const response = await request(documentURL, "GET");
+    if (response.ok) {
+      documents = response.content.documents;
     }
   });
 </script>
 
 <SectionHeader title="Comparison"></SectionHeader>
-<ErrorMessage message={error} plain={true}></ErrorMessage>
-<Diff {diff}></Diff>
+<Tabs>
+  <TabItem open title="JSON diff">
+    <Label class="mb-6">
+      Document 1:
+      <Select id="firstDoc" bind:value={docA} items={selectionOfDocuments}></Select>
+    </Label>
+    <Label>
+      Document 2:
+      <Select id="secondDoc" bind:value={docB} items={selectionOfDocuments}></Select>
+    </Label>
+    <Button on:click={compare} class="my-2">Compare</Button>
+    {#if diffDocuments}
+      <JsonDiff {diffDocuments}></JsonDiff>
+    {/if}
+  </TabItem>
+  <TabItem title="Git diff">
+    <Diff></Diff>
+  </TabItem>
+</Tabs>
