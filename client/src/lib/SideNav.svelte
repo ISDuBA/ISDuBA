38d53dfc43e1ff28287c3eff9878575d6cb15d37
koplas <54645365+koplas@users.noreply.github.com>
2024-04-15 08:39:16 +0000

    Fix format

--- client/src/lib/SideNav.svelte
+++ client/src/lib/SideNav.svelte
@@ -12,7 +12,7 @@
   import { Heading, Sidebar, SidebarWrapper, SidebarGroup, SidebarItem } from "flowbite-svelte";
   import { appStore } from "$lib/store";
   import { page } from "$app/stores";
-  import { PUBLIC_KEYCLOAK_URL } from "$env/static/public"
+  import { PUBLIC_KEYCLOAK_URL } from "$env/static/public";
   async function logout() {
     $appStore.app.keycloak.logout();
   }
@@ -28,10 +28,10 @@
     "flex items-center p-2 text-base font-normal text-primary-900 bg-primary-200 dark:bg-primary-700 dark:text-white hover:bg-primary-100 dark:hover:bg-gray-700";
   let nonActiveClass =
     "flex items-center p-2 text-base font-normal text-white dark:text-white hover:bg-primary-100 hover:text-primary-900";
-  let profileUrl = PUBLIC_KEYCLOAK_URL+"/realms/isduba/account/#/"
+  let profileUrl = PUBLIC_KEYCLOAK_URL + "/realms/isduba/account/#/";
 </script>
 
-<Sidebar class="bg-primary-700 h-screen p-2" {activeUrl} {activeClass} {nonActiveClass}>
+<Sidebar class="h-screen bg-primary-700 p-2" {activeUrl} {activeClass} {nonActiveClass}>
   <SidebarWrapper class="bg-primary-700">
     <Heading class="mb-6 text-white">ISDuBA</Heading>
     <SidebarGroup class="bg-primary-700">
@@ -72,11 +72,7 @@
             <i class="bx bx-cog"></i>
           </svelte:fragment>
         </SidebarItem>
-        <SidebarItem
-          label="Profile"
-          target="_blank"
-          href={profileUrl}
-        >
+        <SidebarItem label="Profile" target="_blank" href={profileUrl}>
           <svelte:fragment slot="icon">
             <i class="bx bx-user"></i>
           </svelte:fragment>
