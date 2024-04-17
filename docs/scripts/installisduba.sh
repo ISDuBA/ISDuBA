3422a1c8ad83ff1a4465f34128079099e86157ff
JanHoefelmeyer <hoefelmeyer.jan@gmail.com>
2024-04-17 14:41:40 +0200

    add copying of .env file

--- docs/scripts/installisduba.sh
+++ docs/scripts/installisduba.sh
@@ -23,4 +23,4 @@ cp docs/example_isdubad.toml isdubad.toml
 echo "Successfully created example-configuration isdubad.toml."
 
 cp client/.env.example client/.env
-echo "Successfully created client-env."
+echo "Successfully created env file."
