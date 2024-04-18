9286528b5181947637701cbd75e46e81302cc35b
JanHoefelmeyer <hoefelmeyer.jan@gmail.com>
2024-04-18 12:13:05 +0200

    Catch if a wrong branch has been entered

--- docs/scripts/installall.sh
+++ docs/scripts/installall.sh
@@ -14,6 +14,8 @@ cd "$HOME"
 
 # TODO: Check whether sudo is necessary where used.
 
+sudo apt-get update
+
 sudo apt-get install git -y # Needed to work with github repositories
 
 # If repository already exists, update it instead of cloning it
@@ -24,8 +26,15 @@ else
   cd ISDuBA/docs/scripts
   git pull
 fi
-if [ ! -z "$1" ]; then
-  git checkout "$1"
+if [ ! -z "$1" ]; then # if a branch was given as a parameter
+  BRANCH=$(git ls-remote --heads origin "refs/heads/$1" | wc -w) # 0 if branch does not exist
+  if [ "$BRANCH" = "0"  ]; then
+    echo "Could not find branch $1. Aborting..."
+  else
+    git checkout "$1"
+    . setup.sh # Execute all the other setup scripts
+  fi
+else
+  . setup.sh # Execute all the other setup scripts
 fi
 
-./setup.sh # Execute all the other setup scripts
