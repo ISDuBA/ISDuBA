4cac53be7d6852e954ce1b8113c5b7ec3358e218
JanHoefelmeyer <hoefelmeyer.jan@gmail.com>
2024-04-18 13:09:46 +0200

    Installall: Add help option, refactor

--- docs/scripts/installall.sh
+++ docs/scripts/installall.sh
@@ -10,10 +10,16 @@
 
 set -e # to exit if a command in the script fails
 
-cd "$HOME"
+# Help function if --help was called
+help() {
+echo "Usage: installall.sh [--help] [branch name]"
+echo "where:"
+echo "  --help       show this help text"
+echo "  branch name  set up ISDuBA on the 'branch name' branch instead of main"
+}
 
-# TODO: Check whether sudo is necessary where used.
-
+# update, install git and get the repository
+prepare() {
 sudo apt-get update
 
 sudo apt-get install git -y # Needed to work with github repositories
@@ -26,15 +32,24 @@ else
   cd ISDuBA/docs/scripts
   git pull
 fi
-if [ ! -z "$1" ]; then # if a branch was given as a parameter
-  BRANCH=$(git ls-remote --heads origin "refs/heads/$1" | wc -w) # 0 if branch does not exist
-  if [ "$BRANCH" = "0"  ]; then
-    echo "Could not find branch $1. Aborting..."
+}
+
+# TODO: Check whether sudo is necessary where used.
+
+if [ ! -z "$1" ]; then # if a an argument was given
+  if [ "$1" = "--help" ]; then
+    help
   else
-    git checkout "$1"
-    . setup.sh # Execute all the other setup scripts
+    prepare
+    BRANCH=$(git ls-remote --heads origin "refs/heads/$1" | wc -w) # 0 if branch does not exist
+    if [ "$BRANCH" = "0"  ]; then
+      echo "Could not find branch $1. Aborting..."
+    else
+      git checkout "$1"
+      ./setup.sh # Execute all the other setup scripts
+    fi
   fi
 else
-  . setup.sh # Execute all the other setup scripts
+  prepare
+  ./setup.sh # Execute all the other setup scripts
 fi
-
