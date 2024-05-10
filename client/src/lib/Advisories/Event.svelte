7c73f139553383601588e78d72e6e30366a33a54
koplas <54645365+koplas@users.noreply.github.com>
2024-05-10 12:24:49 +0200

    Pretty print event types

--- client/src/lib/Advisories/Event.svelte
+++ client/src/lib/Advisories/Event.svelte
@@ -12,12 +12,45 @@
   import { Label, P, TimelineItem } from "flowbite-svelte";
 
   export let event: any;
+
+  const getEventDescription = (e: any) => {
+    let msg: string = "";
+    switch (e.event_type) {
+      case "import_document":
+        msg = "Document imported";
+        if (e.actor) {
+          msg += " by " + e.actor;
+        }
+        break;
+      case "state_change":
+        if (e.actor) {
+          msg = e.actor + " changed status to: " + e.state;
+        } else {
+          msg = "Status changed to: " + e.state;
+        }
+        break;
+      case "add_comment":
+        if (e.actor) {
+          msg = e.actor + " added a comment";
+        } else {
+          msg = "A comment has been added";
+        }
+        break;
+      case "change_comment":
+        if (e.actor) {
+          msg = e.actor + " edited a comment";
+        } else {
+          msg = "A comment has been edited";
+        }
+        break;
+      default:
+        msg = e.event_type;
+    }
+    return msg;
+  };
 </script>
 
 <TimelineItem classLi="mb-4 ms-4" date={`${new Date(event.time).toISOString()}`}>
-  <P class="mb-2">
-    {event.event_type}
-    <small>({event.actor})</small>
-  </P>
+  <P class="mb-2">{getEventDescription(event)}</P>
   <Label class="text-xs text-slate-400">State: {event.state}</Label>
 </TimelineItem>
