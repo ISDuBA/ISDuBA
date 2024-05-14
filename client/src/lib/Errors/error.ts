7e74afd4fc5ac3e880d670a7a410399127cb5d45
Thomas Junk <thomas.junk@intevation.de>
2024-05-14 09:12:45 +0200

    fix: Add message to re-login in case of 401

--- client/src/lib/Errors/error.ts
+++ client/src/lib/Errors/error.ts
@@ -13,7 +13,8 @@ const contactAdmin = `Please contact an administrator.`;
 const ERRORMESSAGES: any = {
   "500": `An error occured on the server. ${contactAdmin}`,
   "400": `The request sent from the client could not be understood. ${contactAdmin}`,
-  "401": `You are not allowed to do this. ${contactAdmin}`,
+  "401": `You are unauthorized. Please re-login.`,
+  "402": `You are not allowed to do this. ${contactAdmin}`,
   "783": `The response from the server is not parsable. ${contactAdmin}`
 };
 
