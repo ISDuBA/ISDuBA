50bf776a7e8cdfebdc8c7ea21139a84efbbef4eb
Christoph Klassen <christoph.klassen@intevation.de>
2024-05-16 06:05:31 +0000

    Show table with queries only if there are queries

--- client/src/lib/Configuration/UserDefinedQueries/Overview.svelte
+++ client/src/lib/Configuration/UserDefinedQueries/Overview.svelte
@@ -41,34 +41,36 @@
 <Button class="mb-6 mt-3" href="/#/configuration/userqueries"
   ><i class="bx bx-plus"></i>New query</Button
 >
-<div class="flex flex-row">
-  <div class="mb-12 w-1/3">
-    <Table hoverable={true} noborder={true}>
-      <TableHead class="cursor-pointer">
-        <TableHeadCell padding={tablePadding} on:click={() => {}}
-          >Name<i
-            class:bx={true}
-            class:bx-caret-up={orderBy == "name"}
-            class:bx-caret-down={orderBy == "-name"}
-          ></i></TableHeadCell
-        >
-        <TableHeadCell padding={tablePadding} on:click={() => {}}
-          >Description<i
-            class:bx={true}
-            class:bx-caret-up={orderBy == "description"}
-            class:bx-caret-down={orderBy == "-description"}
-          ></i>
-        </TableHeadCell>
-      </TableHead>
-      <TableBody>
-        {#each queries as query}
-          <TableBodyRow class="cursor-pointer">
-            <TableBodyCell>{query.name ?? "-"}</TableBodyCell>
-            <TableBodyCell>{query.description ?? "-"}</TableBodyCell>
-          </TableBodyRow>
-        {/each}
-      </TableBody>
-    </Table>
-    <ErrorMessage message={errorMessage}></ErrorMessage>
+{#if queries.length > 0}
+  <div class="flex flex-row">
+    <div class="mb-12 w-1/3">
+      <Table hoverable={true} noborder={true}>
+        <TableHead class="cursor-pointer">
+          <TableHeadCell padding={tablePadding} on:click={() => {}}
+            >Name<i
+              class:bx={true}
+              class:bx-caret-up={orderBy == "name"}
+              class:bx-caret-down={orderBy == "-name"}
+            ></i></TableHeadCell
+          >
+          <TableHeadCell padding={tablePadding} on:click={() => {}}
+            >Description<i
+              class:bx={true}
+              class:bx-caret-up={orderBy == "description"}
+              class:bx-caret-down={orderBy == "-description"}
+            ></i>
+          </TableHeadCell>
+        </TableHead>
+        <TableBody>
+          {#each queries as query}
+            <TableBodyRow class="cursor-pointer">
+              <TableBodyCell>{query.name ?? "-"}</TableBodyCell>
+              <TableBodyCell>{query.description ?? "-"}</TableBodyCell>
+            </TableBodyRow>
+          {/each}
+        </TableBody>
+      </Table>
+      <ErrorMessage message={errorMessage}></ErrorMessage>
+    </div>
   </div>
-</div>
+{/if}
