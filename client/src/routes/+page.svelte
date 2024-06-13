3c0dc8bbeb2aac56229b47710ffcfcdabe245785
Thomas Junk <thomas.junk@intevation.de>
2024-06-13 09:48:54 +0200

    layout: sprinkle of fairy dust

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -166,7 +166,7 @@
   <div>
     <SideNav></SideNav>
   </div>
-  <main class="max-h-screen w-full bg-white p-6">
+  <main class="max-h-screen w-full overflow-auto bg-white p-6">
     {#if $appStore.app.userManager}
       <Router {routes} on:conditionsFailed={conditionsFailed} />
     {/if}
