29747915a464b837a59d79ffbcf355db6dc88258
Christoph Klassen <christoph.klassen@intevation.de>
2024-07-02 14:13:54 +0000

    Remove redundant selection elements

--- client/src/lib/Diff/DiffPage.svelte
+++ client/src/lib/Diff/DiffPage.svelte
@@ -11,22 +11,12 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import SectionHeader from "$lib/SectionHeader.svelte";
-  import { Button, Label, Select } from "flowbite-svelte";
   import JsonDiff from "./JsonDiff.svelte";
   import { appStore } from "$lib/store";
   $: $appStore.app.diff.docA, compare();
   $: $appStore.app.diff.docB, compare();
 
-  let documents: any[] = [];
-  $: selectionOfDocuments = documents.map((doc) => {
-    return {
-      name: `${doc.tracking_id} - Version ${doc.version}`,
-      value: doc
-    };
-  });
   let diffDocuments: any;
-  let docA: any;
-  let docB: any;
   let title: string;
 
   const compare = () => {
@@ -50,15 +40,6 @@
 
 <SectionHeader title="Comparison"></SectionHeader>
 <hr class="mb-6" />
-<Label class="mb-6">
-  Document 1:
-  <Select id="firstDoc" bind:value={docA} items={selectionOfDocuments}></Select>
-</Label>
-<Label>
-  Document 2:
-  <Select id="secondDoc" bind:value={docB} items={selectionOfDocuments}></Select>
-</Label>
-<Button on:click={compare} class="my-2">Compare</Button>
 {#if diffDocuments}
   <JsonDiff {diffDocuments} {title}></JsonDiff>
 {/if}
