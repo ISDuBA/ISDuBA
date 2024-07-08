ccfca74e4cd94d893aa903f602d14fd15a41d228
Thomas Junk <thomas.junk@intevation.de>
2024-07-08 12:08:52 +0200

    feat: rework general section advisory view

--- client/src/lib/Advisories/CSAFWebview/general/General.svelte
+++ client/src/lib/Advisories/CSAFWebview/general/General.svelte
@@ -24,7 +24,6 @@
   $: lang = $appStore.webview.doc?.lang;
   $: sourceLang = $appStore.webview.doc?.sourceLang;
   $: csafVersion = $appStore.webview.doc?.csafVersion;
-  $: id = $appStore.webview.doc?.id;
   $: published = $appStore.webview.doc?.published;
   $: lastUpdate = $appStore.webview.doc?.lastUpdate;
   $: status = $appStore.webview.doc?.status;
@@ -40,118 +39,110 @@
     appStore.setSingleErrorMsg("Are you sure the URL refers to a CSAF document?");
   }
   const cellStyleValue = "px-6 py-0";
-  const cellStyleKey = "py-0";
+  const cellStyleKey = "w-40 py-0";
 </script>
 
 <div class="2xl:w-max">
-  <Table noborder>
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>ID</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{id}</TableBodyCell>
-    </TableBodyRow>
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>CSAF-Version</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{csafVersion}</TableBodyCell>
-    </TableBodyRow>
-    {#if $appStore.webview.doc?.aggregateSeverity}
-      <TableBodyRow>
-        <TableBodyCell tdClass={cellStyleKey}>Aggregate severity text</TableBodyCell>
-        <TableBodyCell tdClass={cellStyleValue}>
-          <span>{$appStore.webview.doc?.aggregateSeverity.text}</span></TableBodyCell
-        >
-      </TableBodyRow>
-      {#if $appStore.webview.doc?.aggregateSeverity.namespace}
-        <TableBodyRow>
-          <TableBodyCell tdClass={cellStyleKey}>Aggregate severity namespace</TableBodyCell>
-          <TableBodyCell tdClass={cellStyleValue}
-            ><span>{$appStore.webview.doc?.aggregateSeverity.namespace}</span></TableBodyCell
-          >
-        </TableBodyRow>
-      {/if}
-    {/if}
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>Category</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{category}</TableBodyCell>
-    </TableBodyRow>
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>Title</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{title}</TableBodyCell>
-    </TableBodyRow>
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>Publisher name</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{publisherName}</TableBodyCell>
-    </TableBodyRow>
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>Publisher category</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{publisherCategory}</TableBodyCell>
-    </TableBodyRow>
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>Publisher namespace</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{publisherNamespace}</TableBodyCell>
-    </TableBodyRow>
-    {#if publisherIssuingAuthority}
-      <TableBodyRow>
-        <TableBodyCell tdClass={cellStyleKey}>Publisher issuing authority</TableBodyCell>
-        <TableBodyCell tdClass={cellStyleValue}>{publisherIssuingAuthority}</TableBodyCell>
-      </TableBodyRow>
-    {/if}
-    {#if publisherContactDetails}
-      <TableBodyRow>
-        <TableBodyCell tdClass={cellStyleKey}>Publisher contact details</TableBodyCell>
-        <TableBodyCell tdClass={cellStyleValue}>{publisherContactDetails}</TableBodyCell>
-      </TableBodyRow>
-    {/if}
-    {#if lang}
-      <TableBodyRow>
-        <TableBodyCell tdClass={cellStyleKey}>Language</TableBodyCell>
-        <TableBodyCell tdClass={cellStyleValue}>{lang}</TableBodyCell>
-      </TableBodyRow>
-    {/if}
-    {#if sourceLang}
-      <TableBodyRow>
-        <TableBodyCell tdClass={cellStyleKey}>Source lang</TableBodyCell>
-        <TableBodyCell tdClass={cellStyleValue}>{sourceLang}</TableBodyCell>
-      </TableBodyRow>
-    {/if}
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>Published</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{published}</TableBodyCell>
-    </TableBodyRow>
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>Last update</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{lastUpdate}</TableBodyCell>
-    </TableBodyRow>
-    <TableBodyRow>
-      <TableBodyCell tdClass={cellStyleKey}>Tracking Version</TableBodyCell>
-      <TableBodyCell tdClass={cellStyleValue}>{trackingVersion}</TableBodyCell>
-    </TableBodyRow>
+  <div class="mb-3">
+    <span class="text-xl">{title} </span>
     {#if $appStore.webview.doc?.status !== Status.final}
-      <TableBodyRow>
-        <TableBodyCell tdClass={cellStyleKey}>Status</TableBodyCell>
-        <TableBodyCell tdClass={cellStyleValue}>{status}</TableBodyCell>
-      </TableBodyRow>
+      <span class="ml-3 text-lg text-gray-400">{status}</span>
     {/if}
-    {#if generator}
-      <TableBodyRow>
-        <TableBodyCell tdClass={cellStyleKey}>Generator engine</TableBodyCell>
-        <TableBodyCell tdClass={cellStyleValue}
-          ><span>{$appStore.webview.doc?.generator?.engine.name}</span></TableBodyCell
-        >
-      </TableBodyRow>
-    {/if}
-    {#if generator?.engine?.version}
-      <TableBodyRow>
-        <TableBodyCell tdClass={cellStyleKey}>Generator engine version</TableBodyCell>
-        <TableBodyCell tdClass={cellStyleValue}
-          ><span>{$appStore.webview.doc?.generator?.engine.version}</span></TableBodyCell
-        >
-      </TableBodyRow>
-    {/if}
-    {#if generator?.date}
-      <TableBodyRow>
-        <TableBodyCell tdClass={cellStyleKey}>Generator date</TableBodyCell>
-        <TableBodyCell tdClass={cellStyleValue}><span>{generator?.date}</span></TableBodyCell>
-      </TableBodyRow>
-    {/if}
-  </Table>
+  </div>
+  <div class="flex flex-row flex-wrap">
+    <div class="flex flex-col">
+      <Table noborder>
+        <TableBodyRow>
+          <TableBodyCell tdClass={cellStyleKey}>Publisher name</TableBodyCell>
+          <TableBodyCell tdClass={cellStyleValue}>{publisherName}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+          <TableBodyCell tdClass={cellStyleKey}>Publisher namespace</TableBodyCell>
+          <TableBodyCell tdClass={cellStyleValue}>{publisherNamespace}</TableBodyCell>
+        </TableBodyRow>
+        {#if publisherContactDetails}
+          <TableBodyRow>
+            <TableBodyCell tdClass={cellStyleKey}>Publisher contact details</TableBodyCell>
+            <TableBodyCell tdClass={cellStyleValue}>{publisherContactDetails}</TableBodyCell>
+          </TableBodyRow>
+        {/if}
+        {#if publisherIssuingAuthority}
+          <TableBodyRow>
+            <TableBodyCell tdClass={cellStyleKey}>Publisher issuing authority</TableBodyCell>
+            <TableBodyCell tdClass={cellStyleValue}>{publisherIssuingAuthority}</TableBodyCell>
+          </TableBodyRow>
+        {/if}
+        <TableBodyRow>
+          <TableBodyCell tdClass={cellStyleKey}>Publisher category</TableBodyCell>
+          <TableBodyCell tdClass={cellStyleValue}>{publisherCategory}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+          <TableBodyCell tdClass={cellStyleKey}>Published</TableBodyCell>
+          <TableBodyCell tdClass={cellStyleValue}>{published}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+          <TableBodyCell tdClass={cellStyleKey}>Last update</TableBodyCell>
+          <TableBodyCell tdClass={cellStyleValue}>{lastUpdate}</TableBodyCell>
+        </TableBodyRow>
+      </Table>
+    </div>
+    <div class="flex flex-col">
+      <Table noborder>
+        <TableBodyRow>
+          <TableBodyCell tdClass={cellStyleKey}>CSAF-Version</TableBodyCell>
+          <TableBodyCell tdClass={cellStyleValue}>{csafVersion}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+          <TableBodyCell tdClass={cellStyleKey}>Category</TableBodyCell>
+          <TableBodyCell tdClass={cellStyleValue}>{category}</TableBodyCell>
+        </TableBodyRow>
+        {#if $appStore.webview.doc?.aggregateSeverity}
+          <TableBodyRow>
+            <TableBodyCell tdClass={cellStyleKey}>Aggregate severity text</TableBodyCell>
+            <TableBodyCell tdClass={cellStyleValue}>
+              <span>{$appStore.webview.doc?.aggregateSeverity.text}</span></TableBodyCell
+            >
+          </TableBodyRow>
+          {#if $appStore.webview.doc?.aggregateSeverity.namespace}
+            <TableBodyRow>
+              <TableBodyCell tdClass={cellStyleKey}>Aggregate severity namespace</TableBodyCell>
+              <TableBodyCell tdClass={cellStyleValue}
+                ><span>{$appStore.webview.doc?.aggregateSeverity.namespace}</span></TableBodyCell
+              >
+            </TableBodyRow>
+          {/if}
+        {/if}
+        {#if lang}
+          <TableBodyRow>
+            <TableBodyCell tdClass={cellStyleKey}>Language</TableBodyCell>
+            <TableBodyCell tdClass={cellStyleValue}>{lang}</TableBodyCell>
+          </TableBodyRow>
+        {/if}
+        {#if sourceLang}
+          <TableBodyRow>
+            <TableBodyCell tdClass={cellStyleKey}>Source lang</TableBodyCell>
+            <TableBodyCell tdClass={cellStyleValue}>{sourceLang}</TableBodyCell>
+          </TableBodyRow>
+        {/if}
+        <TableBodyRow>
+          <TableBodyCell tdClass={cellStyleKey}>Tracking Version</TableBodyCell>
+          <TableBodyCell tdClass={cellStyleValue}>{trackingVersion}</TableBodyCell>
+        </TableBodyRow>
+      </Table>
+    </div>
+  </div>
+  <div class="mt-3 flex flex-row">
+    <span class="text-sm text-gray-400">
+      Generator:
+      {#if generator}
+        {$appStore.webview.doc?.generator?.engine.name}
+      {/if}
+      {#if generator?.engine?.version}
+        {$appStore.webview.doc?.generator?.engine.version}
+      {/if}
+      {#if generator?.date}
+        {generator?.date.replace("T", " ").split(".")[0]}
+      {/if}
+    </span>
+  </div>
 </div>
