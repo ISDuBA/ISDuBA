d8763aa9e87fe10070f0593feecf2ea5e529057f
koplas <54645365+koplas@users.noreply.github.com>
2024-07-24 14:20:34 +0200

    Add aggregator table

--- client/src/lib/Sources/Overview.svelte
+++ client/src/lib/Sources/Overview.svelte
@@ -17,18 +17,26 @@
 
   let sourceError = "";
   let loadingSources = false;
-  let sources: any[] = [];
+  let sources: any[] = [
+    {
+      name: "RedHat",
+      source: "redhat.com",
+      interval: "every 12 hours",
+      lastRun: new Date(Date.now() - 1000 * 60)
+    }
+  ];
+  let aggregators: any[] = [
+    {
+      name: "BSI Lister",
+      source: "https://wid.cert-bund.de/.well-known/csaf-aggregator/aggregator.json",
+      interval: "every 12 hours",
+      lastRun: new Date(Date.now() - 1000 * 60 * 60)
+    }
+  ];
   let modalOpen = false;
   let modalMessage: string;
   let modalTitle: string;
   let modalCallback: any;
-
-  sources.push({
-    name: "RedHat",
-    source: "redhat.com",
-    interval: "every 12 hours",
-    lastRun: new Date(Date.now() - 1000 * 60)
-  });
 
   const getRelativeTime = (date: Date) => {
     const now = Date.now();
@@ -141,6 +149,87 @@
     <Button href="/#/sources/job/new" class="mb-2" color="primary" size="xs">
       <i class="bx bx-plus"></i>
       <span>Add new source</span>
+    </Button>
+    <ErrorMessage message={sourceError}></ErrorMessage>
+  </div>
+</CustomTable>
+
+<CustomTable
+  title="Aggregators"
+  headers={[
+    {
+      label: "Name",
+      attribute: "name"
+    },
+    {
+      label: "URL",
+      attribute: "source"
+    },
+    {
+      label: "Interval",
+      attribute: "interval"
+    },
+    {
+      label: "Last checked",
+      attribute: "lastRun"
+    },
+    {
+      label: "Actions",
+      attribute: undefined
+    }
+  ]}
+>
+  {#each aggregators as aggregator, index (index)}
+    <tr on:blur={() => {}} on:focus={() => {}} class="cursor-pointer">
+      <TableBodyCell {tdClass}>{aggregator.name}</TableBodyCell>
+      <TableBodyCell {tdClass}>{aggregator.source}</TableBodyCell>
+      <TableBodyCell {tdClass}>{aggregator.interval}</TableBodyCell>
+      <TableBodyCell {tdClass}>{getRelativeTime(aggregator.lastRun)}</TableBodyCell>
+      <td>
+        <Button
+          on:click={() => {
+            push(`/sources/job/${aggregator.id}`);
+          }}
+          title={`Edit Source "${aggregator.name}"`}
+          class="border-0 p-2"
+          color="light"
+        >
+          <i class="bx bx-edit-alt text-xl"></i>
+        </Button>
+        <Button
+          on:click={(event) => {
+            event.stopPropagation();
+            modalMessage = "Are you sure you want to delete this source?";
+            modalTitle = `Source ${aggregator.name}`;
+            modalOpen = true;
+          }}
+          title={`Remove Source "${aggregator.name}"`}
+          class="border-0 p-2"
+          color="light"
+        >
+          <i class="bx bx-trash text-xl text-red-500"></i>
+        </Button>
+        <Button
+          on:click={(event) => {
+            event.stopPropagation();
+          }}
+          title={`Download from source "${aggregator.name}"`}
+          class="border-0 p-2"
+          color="light"
+        >
+          <i class="bx bx-play-circle text-xl"></i>
+        </Button>
+      </td>
+    </tr>
+  {/each}
+  <div slot="bottom">
+    <div class:hidden={!loadingSources} class:mb-4={true}>
+      Loading ...
+      <Spinner color="gray" size="4"></Spinner>
+    </div>
+    <Button href="/#/sources/job/new" class="mb-2" color="primary" size="xs">
+      <i class="bx bx-plus"></i>
+      <span>Add new lister</span>
     </Button>
     <ErrorMessage message={sourceError}></ErrorMessage>
   </div>
