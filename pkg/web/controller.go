068a2aafa84e63331c00a7b0d30236b5e3a4940c
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2024-07-27 23:58:08 +0200

    Add stubs for sources endpoints.

--- pkg/web/controller.go
+++ pkg/web/controller.go
@@ -81,6 +81,8 @@ func (c *Controller) Bind() http.Handler {
 		authEdRe   = authRoles(models.Editor, models.Reviewer)
 		authEdReAu = authRoles(models.Editor, models.Reviewer, models.Auditor)
 		authEdReAd = authRoles(models.Editor, models.Reviewer, models.Admin)
+		authSM     = authRoles(models.SourceManager)
+		authEdSM   = authRoles(models.Editor, models.SourceManager)
 		authAll    = authRoles(models.Admin, models.Importer, models.Editor,
 			models.Reviewer, models.Auditor, models.SourceManager)
 	)
@@ -139,6 +141,18 @@ func (c *Controller) Bind() http.Handler {
 	api.GET("/view", authAll, c.view)
 
 	// Source manager
+	api.GET("/sources", authEdSM, c.viewSources)
+	api.POST("/sources", authSM, c.createSource)
 	api.GET("/sources/message", authAll, c.defaultMessage)
+	api.DELETE("/sources/:id", authSM, c.deleteSource)
+	api.PUT("/sources/:id", authSM, c.updateSource)
+
+	// Source feeds
+	api.GET("/sources/:id/feeds", authEdSM, c.viewFeeds)
+	api.POST("/sources/:id/feeds", authSM, c.createFeed)
+	api.GET("/sources/:id/feeds/:fid", authEdSM, c.viewFeed)
+	api.DELETE("/sources/:id/feeds/:fid", authSM, c.deleteFeed)
+	api.GET("/sources/:id/feeds/:fid/log", authSM, c.feedLog)
+
 	return r
 }
--- pkg/web/sources.go
+++ pkg/web/sources.go
@@ -14,6 +14,69 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func (c *Controller) viewSources(ctx *gin.Context) {
+	// TODO: Implement me!
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": "'viewSources' not implemented, yet.",
+	})
+}
+
+func (c *Controller) createSource(ctx *gin.Context) {
+	// TODO: Implement me!
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": "'createSource' not implemented, yet.",
+	})
+}
+
+func (c *Controller) deleteSource(ctx *gin.Context) {
+	// TODO: Implement me!
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": "'deleteSource' not implemented, yet.",
+	})
+}
+
+func (c *Controller) updateSource(ctx *gin.Context) {
+	// TODO: Implement me!
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": "'updateSource' not implemented, yet.",
+	})
+}
+
+func (c *Controller) viewFeeds(ctx *gin.Context) {
+	// TODO: Implement me!
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": "'viewFeeds' not implemented, yet.",
+	})
+}
+
+func (c *Controller) createFeed(ctx *gin.Context) {
+	// TODO: Implement me!
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": "'createFeed' not implemented, yet.",
+	})
+}
+
+func (c *Controller) viewFeed(ctx *gin.Context) {
+	// TODO: Implement me!
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": "'viewFeed' not implemented, yet.",
+	})
+}
+
+func (c *Controller) deleteFeed(ctx *gin.Context) {
+	// TODO: Implement me!
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": "'deleteFeed' not implemented, yet.",
+	})
+}
+
+func (c *Controller) feedLog(ctx *gin.Context) {
+	// TODO: Implement me!
+	ctx.JSON(http.StatusInternalServerError, gin.H{
+		"error": "'feedLog' not implemented, yet.",
+	})
+}
+
 // defaultMessage returns the default message.
 func (c *Controller) defaultMessage(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": c.cfg.Sources.DefaultMessage})
