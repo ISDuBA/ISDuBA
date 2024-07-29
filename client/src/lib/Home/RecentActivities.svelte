d73142df5beaa1b91c314d25dee6896c1003b2ae
koplas <54645365+koplas@users.noreply.github.com>
2024-07-29 10:00:41 +0200

    Only load events if allowed

--- client/src/lib/Home/RecentActivities.svelte
+++ client/src/lib/Home/RecentActivities.svelte
@@ -257,7 +257,7 @@
   });
 </script>
 
-{#if $appStore.app.isUserLoggedIn}
+{#if ($appStore.app.isUserLoggedIn && appStore.isEditor()) || appStore.isReviewer() || appStore.isAuditor()}
   <div class="flex w-1/2 max-w-[50%] flex-col gap-4">
     <SectionHeader title="Recent activities"></SectionHeader>
     <div class="grid grid-cols-[repeat(auto-fit,_minmax(200pt,_1fr))] gap-6">
