37618faf1a5965f253935ea70c7f51f298f584b1
koplas <54645365+koplas@users.noreply.github.com>
2024-09-19 15:44:02 +0200

    Reapply "Use custom url checker"
    This reverts commit 24b1ee028651a5499512e34a2f17467d19e23cf8.

--- client/src/lib/Sources/SourceCreator.svelte
+++ client/src/lib/Sources/SourceCreator.svelte
@@ -35,7 +35,7 @@
   let sourceForm: any;
   let updateSourceForm: any;
 
-  let validUrl: boolean | null = null;
+  let validUrl: boolean | null = false;
   let urlColor: "red" | "green" | "base" = "base";
   $: if (validUrl !== undefined) {
     if (validUrl === null) {
@@ -83,7 +83,31 @@
     }
   };
 
-  const checkUrl = async () => {};
+  const checkUrl = async () => {
+    if (source.url.startsWith("https://") && source.url.endsWith("provider-metadata.json")) {
+      validUrl = null;
+      return;
+    }
+    try {
+      new URL(source.url);
+      validUrl = false;
+      return;
+    } catch {
+      let domain = source.url;
+      if (domain.charAt(domain.length - 1) == ".") {
+        domain = domain.slice(0, -1);
+      }
+      let split = source.url.split(".");
+
+      let first = split.pop() ?? [];
+      let second = split.pop() ?? [];
+      if (first.length < 2 || second.length < 1) {
+        validUrl = false;
+        return;
+      }
+      validUrl = null;
+    }
+  };
 
   const saveAll = async () => {
     updateSourceForm();
@@ -161,7 +185,7 @@
       Loading ...
       <Spinner color="gray" size="4"></Spinner>
     </div>
-    <Button type="submit" color="light">
+    <Button type="submit" color="light" disabled={validUrl === false}>
       <i class="bx bx-check me-2"></i>
       <span>Search and load provider metadata</span>
     </Button>
