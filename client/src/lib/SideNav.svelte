c6b9d1d749e830231a7e721fe1e9f40693523966
koplas <54645365+koplas@users.noreply.github.com>
2024-10-01 10:08:46 +0200

    Hide source page if user has no permission

--- client/src/lib/SideNav.svelte
+++ client/src/lib/SideNav.svelte
@@ -86,11 +86,13 @@
                 <i class="bx bx-spreadsheet"></i>
               </svelte:fragment>
             </SidebarItem>
-            <SidebarItem class="px-6 py-2.5" label="Sources" href="/#/sources">
-              <svelte:fragment slot="icon">
-                <i class="bx bx-git-repo-forked"></i>
-              </svelte:fragment>
-            </SidebarItem>
+            {#if appStore.isEditor() || appStore.isSourceManager()}
+              <SidebarItem class="px-6 py-2.5" label="Sources" href="/#/sources">
+                <svelte:fragment slot="icon">
+                  <i class="bx bx-git-repo-forked"></i>
+                </svelte:fragment>
+              </SidebarItem>
+            {/if}
             <SidebarItem
               class="px-6 py-2.5"
               label="Statistics"
