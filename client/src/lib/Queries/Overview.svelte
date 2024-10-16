22c28adb498a72731997e07212096d68358d3f5a
JanHoefelmeyer <jan.hoefelmeyer@intevation.de>
2024-10-16 13:32:29 +0200

    Fix: Formatting

--- client/src/lib/Queries/Overview.svelte
+++ client/src/lib/Queries/Overview.svelte
@@ -101,7 +101,7 @@
     let failed = false;
     isCloning = true;
     // Clone the special queries
-    for (let i = globalRelevantQueries.length-1; i >= 0; i--) {
+    for (let i = globalRelevantQueries.length - 1; i >= 0; i--) {
       const queryToClone = globalRelevantQueries[i];
       if (queryToClone) {
         queryToClone.global = false;
