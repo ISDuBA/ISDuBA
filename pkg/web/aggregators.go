6cf4c2ca7137fb66f12c0f2d1e955d816f39e2e0
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2024-10-28 06:59:44 +0100

    Add GET /api/aggregators

--- pkg/web/aggregators.go
+++ pkg/web/aggregators.go
@@ -9,27 +9,54 @@
 package web
 
 import (
+	"context"
+	"log/slog"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func (c *Controller) viewAggregators(ctx *gin.Context) {
-	// TODO: Implement me!
-	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Not implemted, yet!"})
+	type aggregator struct {
+		ID   int64  `json:"id"`
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	}
+	var list []aggregator
+	const sql = `SELECT id, name, url FROM aggregators ORDER by name`
+	if err := c.db.Run(
+		ctx.Request.Context(),
+		func(rctx context.Context, conn *pgxpool.Conn) error {
+			rows, _ := conn.Query(rctx, sql)
+			var err error
+			list, err = pgx.CollectRows(rows, func(row pgx.CollectableRow) (aggregator, error) {
+				var a aggregator
+				err := row.Scan(&a.ID, &a.Name, &a.URL)
+				return a, err
+			})
+			return err
+		}, 0,
+	); err != nil {
+		slog.Error("fetching aggregators failed", "error", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, list)
 }
 
 func (c *Controller) viewAggregator(ctx *gin.Context) {
 	// TODO: Implement me!
-	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Not implemted, yet!"})
+	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Not implemented, yet!"})
 }
 
 func (c *Controller) createAggregator(ctx *gin.Context) {
 	// TODO: Implement me!
-	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Not implemted, yet!"})
+	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Not implemented, yet!"})
 }
 
 func (c *Controller) deleteAggregator(ctx *gin.Context) {
 	// TODO: Implement me!
-	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Not implemted, yet!"})
+	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Not implemented, yet!"})
 }
