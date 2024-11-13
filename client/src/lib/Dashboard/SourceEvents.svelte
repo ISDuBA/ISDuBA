222ebaf937e8ccb7828b8e2591f5fbcada41ad1d
Christoph Klassen <christoph.klassen@intevation.de>
2024-11-13 20:08:55 +0000

    Add space between source changes and stats

--- client/src/lib/Dashboard/SourceEvents.svelte
+++ client/src/lib/Dashboard/SourceEvents.svelte
@@ -65,7 +65,7 @@
 </script>
 
 {#if $appStore.app.isUserLoggedIn && appStore.isSourceManager()}
-  <div class="flex flex-col gap-4 md:w-[46%] md:max-w-[46%]">
+  <div class="mb-8 flex flex-col gap-4 md:w-[46%] md:max-w-[46%]">
     <SectionHeader title="Changed sources"></SectionHeader>
     <div class="grid grid-cols-[repeat(auto-fit,_minmax(200pt,_1fr))] gap-6">
       {#if isLoading}
