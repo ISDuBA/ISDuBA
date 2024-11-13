8dee7fbbcd8e49e88a75000bd6bfbdf719239591
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2024-11-13 17:43:44 +0100

    Ignore empty patterns in regexes in download filtering

--- pkg/sources/util.go
+++ pkg/sources/util.go
@@ -30,14 +30,18 @@ func AsRegexps(s []string) ([]*regexp.Regexp, error) {
 	if s == nil {
 		return nil, nil
 	}
-	slice := make([]*regexp.Regexp, len(s))
-	for i, x := range s {
+	slice := make([]*regexp.Regexp, 0, len(s))
+	for _, x := range s {
+		// Ignore empty strings.
+		if x == "" {
+			continue
+		}
 		re, err := regexp.Compile(x)
 		if err != nil {
 			return nil, InvalidArgumentError(
 				fmt.Sprintf("ignore pattern %q is not a valid regexp: %v", x, err))
 		}
-		slice[i] = re
+		slice = append(slice, re)
 	}
 	return slice, nil
 }
