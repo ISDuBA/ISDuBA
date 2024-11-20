f60f0850700258190c59d5ca28f51d87c819ba93
koplas <54645365+koplas@users.noreply.github.com>
2024-11-20 11:03:42 +0100

    Migrate Keycloak config and cleanup

--- docker/keycloak/init.sh
+++ docker/keycloak/init.sh
@@ -10,8 +10,8 @@
 
 PATH=/opt/keycloak/bin:$PATH
 
-adminuser=${KEYCLOAK_ADMIN}
-adminpass=${KEYCLOAK_ADMIN_PASSWORD}
+adminuser=${KC_BOOTSTRAP_ADMIN_USERNAME}
+adminpass=${KC_BOOTSTRAP_ADMIN_PASSWORD}
 client_hostname_url=${CLIENT_HOSTNAME_URL}
 
 # log into the master realm with admin rights, token saved in ~/.keycloak/kcadm.config
@@ -26,23 +26,23 @@ echo $id
 kcadm.sh get realms/isduba/clients/$id
 
 kcadm.sh update clients/$id --target-realm=isduba \
-	--set rootUrl=$client_hostname_url \
-	--set "redirectUris=[\"$client_hostname_url/*\"]" \
-	--set 'attributes={
+  --set rootUrl=$client_hostname_url \
+  --set "redirectUris=[\"$client_hostname_url/*\"]" \
+  --set 'attributes={
     "oidc.ciba.grant.enabled" : "false",
     "post.logout.redirect.uris" : "+",
     "oauth2.device.authorization.grant.enabled" : "false",
     "backchannel.logout.session.required" : "true",
     "backchannel.logout.revoke.offline.tokens" : "false" }' \
-	--set 'webOrigins=["*"]' \
-	--set "adminUrl=$client_hostname_url/" \
-	--set publicClient=true \
-	--set standardFlowEnabled=true \
-	--set directAccessGrantsEnabled=true \
-	--set consentRequired=false
+  --set 'webOrigins=["*"]' \
+  --set "adminUrl=$client_hostname_url/" \
+  --set publicClient=true \
+  --set standardFlowEnabled=true \
+  --set directAccessGrantsEnabled=true \
+  --set consentRequired=false
 
 kcadm.sh update clients/$id --target-realm=isduba \
-	--set 'protocolMappers=[ {
+  --set 'protocolMappers=[ {
     "name" : "TLP",
     "protocol" : "openid-connect",
     "protocolMapper" : "oidc-usermodel-attribute-mapper",
@@ -63,28 +63,28 @@ kcadm.sh update clients/$id --target-realm=isduba \
 # Create groups
 WHITE_GREEN_ID=$(kcadm.sh create groups --target-realm=isduba --set name=white-green -i)
 kcadm.sh update groups/$WHITE_GREEN_ID --target-realm isduba \
-	--set 'attributes={
+  --set 'attributes={
     "TLP" : [ "{\"*\": [ \"WHITE\", \"GREEN\" ]}" ]
   }'
 
 # Create roles
 kcadm.sh create roles --target-realm=isduba --set name=editor \
-	--set "description=editor"
+  --set "description=editor"
 
 #creating  a user
 ALEX_ID=$(kcadm.sh create users --target-realm isduba \
-	--set username=alex --set enabled=true \
-	--set firstName=Alex --set lastName=Klein \
-	--set email=test@example.org \
-	--set emailVerified=true \
-	-i)
+  --set username=alex --set enabled=true \
+  --set firstName=Alex --set lastName=Klein \
+  --set email=test@example.org \
+  --set emailVerified=true \
+  -i)
 
 password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
 kcadm.sh set-password --target-realm isduba \
-	--username alex --new-password "$password"
+  --username alex --new-password "$password"
 
 echo -e "\nCreated user 'alex' with password: $password"
 
 kcadm.sh add-roles -r isduba --uusername alex --rolename editor
 kcadm.sh update -r isduba users/$ALEX_ID/groups/$WHITE_GREEN_ID \
-	-s realm=isduba -s userId=$ALEX_ID -s groupId=$WHITE_GREEN_ID -n
+  -s realm=isduba -s userId=$ALEX_ID -s groupId=$WHITE_GREEN_ID -n
