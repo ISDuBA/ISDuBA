8d77eaf5ea3f8861bd23651c4e27eabc299501a2
koplas <pschwabauer@intevation.de>
2024-11-21 10:18:21 +0100

    Only insert default aggregator if not already configured

--- pkg/database/migrations/032-active_aggregators.sql
+++ pkg/database/migrations/032-active_aggregators.sql
@@ -6,6 +6,19 @@
 -- SPDX-FileCopyrightText: 2024 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
 -- Software-Engineering: 2024 Intevation GmbH <https://intevation.de>
 
-ALTER TABLE aggregators ADD COLUMN active bool NOT NULL DEFAULT FALSE;
+ALTER TABLE aggregators
+    ADD COLUMN active bool NOT NULL DEFAULT FALSE;
 
-INSERT INTO aggregators (active, name, url) VALUES (FALSE, 'BSI CSAF Lister', 'https://wid.cert-bund.de/.well-known/csaf-aggregator/aggregator.json');
+DO
+$$
+    DECLARE
+        default_aggregator_url constant varchar = 'https://wid.cert-bund.de/.well-known/csaf-aggregator/aggregator.json';
+    BEGIN
+        IF NOT EXISTS (SELECT 1
+                       FROM aggregators
+                       WHERE url = default_aggregator_url) THEN
+            INSERT INTO aggregators (active, name, url)
+            VALUES (FALSE, 'BSI CSAF Lister', default_aggregator_url);
+        END IF;
+    END
+$$
