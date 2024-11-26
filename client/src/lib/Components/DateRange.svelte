1596d151bf476eb57d84c0fa1147560e285f35d0
Christoph Klassen <christoph.klassen@intevation.de>
2024-11-26 15:48:49 +0000

    refactor: Separate component for time range selection

---
+++ client/src/lib/Components/DateRange.svelte
@@ -0,0 +1,39 @@
+<!--
+ This file is Free Software under the Apache-2.0 License
+ without warranty, see README.md and LICENSES/Apache-2.0.txt for details.
+
+ SPDX-License-Identifier: Apache-2.0
+
+ SPDX-FileCopyrightText: 2024 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+ Software-Engineering: 2024 Intevation GmbH <https://intevation.de>
+-->
+
+<script lang="ts">
+  import { Input, Label } from "flowbite-svelte";
+  import { createEventDispatcher } from "svelte";
+
+  export let from: string | undefined;
+  export let to: string | undefined;
+
+  const dispatch = createEventDispatcher();
+  const uuid = crypto.randomUUID();
+  const fromId = `from-${uuid}`;
+  const toId = `to-${uuid}`;
+
+  const onChange = () => {
+    dispatch("change");
+  };
+</script>
+
+<Label for={fromId}
+  ><span>From:</span>
+  <Input let:props>
+    <input on:change={onChange} id={fromId} type="date" {...props} bind:value={from} />
+  </Input>
+</Label>
+<Label for={toId}
+  ><span>To:</span>
+  <Input let:props>
+    <input on:change={onChange} id={toId} type="date" {...props} bind:value={to} />
+  </Input>
+</Label>
