aa4023ec3f1ba49f81308bdc3c40bfbfcea82f4c
Bernhard Reiter <bernhard@intevation.de>
2026-01-14 16:56:45 +0100

    updating installscripts
     * move keycloak to 26.5.0 from 26.0.0
     * move nodejs to 24 from 20 (as support for 20 runs out in 2026-04)
     * remove superflous `npm run prepare` as this is run by `npm install`
       already.

--- docs/scripts/installplaywright.sh
+++ docs/scripts/installplaywright.sh
@@ -13,7 +13,7 @@ set -e # to exit if a command in the script fails
 # install node
 sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
 sudo curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
+NODE_MAJOR=24
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update && sudo apt-get install nodejs -y
 echo "Successfully installed node."
@@ -23,9 +23,6 @@ cd ../../client # change directory
 # install dependencies
 npm install
 echo "Successfully installed client dependencies."
-
-# enable pre-commit hook
-npm run prepare
 
 sudo npx playwright install --with-deps
 # needs to be rerun for browsers
