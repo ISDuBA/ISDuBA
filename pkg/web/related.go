0234a49e21462b07a40ed30b3c9bb8d68e7e6cc1
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2026-01-15 13:50:29 +0100

    Improved SQ statement a bit. Maybe still broken.

--- pkg/web/related.go
+++ pkg/web/related.go
@@ -25,34 +25,34 @@ const selectCVERelatedSQL = `
 WITH related AS (
    SELECT
      dc.documents_id,
-	 uc.cve,
-	 dc.cve_id
+     uc.cve,
+     dc.cve_id
    FROM documents_cves dc
      JOIN unique_cves uc ON dc.cve_id          = uc.id
-	 JOIN documents docs ON dc.documents_id    = docs.id
-	 JOIN advisories ads ON docs.advisories_id = ads.id
+     JOIN documents docs ON dc.documents_id    = docs.id
+     JOIN advisories ads ON docs.advisories_id = ads.id
    WHERE documents_id = $1 AND %[1]s
 ),
 others AS (
   SELECT
     dc2.documents_id,
-	related.cve
+    related.cve
   FROM documents_cves dc2
     JOIN related ON dc2.cve_id = related.cve_id
   WHERE dc2.documents_id <> $1
-),
-  SELECT
-    others.documents_id,
-	others.cve,
-    ads.state,
-    d.ssvc,
-    d.title,
-    ads.tracking_id
-	ads.publisher
-  FROM others
-    JOIN documents  d   ON others.documents_id = d.id
-	JOIN advisories ads ON d.advisories_id = ads.id
-  WHERE %[1]s
+)
+SELECT
+  others.documents_id,
+  others.cve,
+  ads.state,
+  d.ssvc,
+  d.title,
+  ads.tracking_id,
+  ads.publisher
+FROM others
+  JOIN documents  d   ON others.documents_id = d.id
+  JOIN advisories ads ON d.advisories_id     = ads.id
+WHERE %[1]s
 `
 
 // cveRelatedDocuments is an endpoint that returns the documents
