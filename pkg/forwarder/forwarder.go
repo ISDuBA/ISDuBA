94249c154d99ff81ed8f05363846845c5f041bdf
Sascha L, Teichmann <sascha.teichmann@intevation.de>
2026-02-14 08:47:22 +0100

    load document ids from forwarder queue.

--- pkg/forwarder/forwarder.go
+++ pkg/forwarder/forwarder.go
@@ -12,12 +12,20 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/ISDuBA/ISDuBA/pkg/config"
 	"github.com/ISDuBA/ISDuBA/pkg/database"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+// forwarderWakeupInterval is a saftey net wakeup interval for each
+// forwarder if a ping from the manager is missed some how.
+const forwarderWakeupInterval = 2 * time.Minute
 
 type forwarder struct {
 	cfg     *config.ForwardTarget
@@ -68,8 +76,13 @@ func newForwarder(cfg *config.ForwardTarget, db *database.DB) (*forwarder, error
 }
 
 func (f *forwarder) run(ctx context.Context) {
+	ticker := time.NewTicker(forwarderWakeupInterval)
+	defer ticker.Stop()
 	// TODO: Implement me!
 	for !f.done {
+		if err := f.forward(ctx); err != nil {
+			slog.Error("forwarder has issues", "error", err, "forwarder", f.cfg.URL)
+		}
 		select {
 		case fn := <-f.fns:
 			fn(f)
@@ -79,8 +92,54 @@ func (f *forwarder) run(ctx context.Context) {
 	}
 }
 
+func (f *forwarder) forward(ctx context.Context) error {
+	const docIDsSQL = `` +
+		`SELECT` +
+		` documents_id ` +
+		`FROM forwarders_queue fwq ` +
+		`JOIN forwarders fw ON fwq.forwarders_id = fw.id ` +
+		`WHERE` +
+		` fwq.state IN ('pending', 'failed')` +
+		` AND fw.url = $1 ` +
+		`ORDER BY documents_id DESC ` + // Assuming older docs have lower ids.
+		`LIMIT 20` // Poll in smaller batches.
+
+	for {
+		var docIDs []int64
+		if err := f.db.Run(
+			ctx,
+			func(rctx context.Context, conn *pgxpool.Conn) error {
+				rows, _ := conn.Query(rctx, docIDsSQL, f.cfg.URL)
+				var err error
+				docIDs, err = pgx.CollectRows(
+					rows,
+					func(row pgx.CollectableRow) (int64, error) {
+						var docID int64
+						err := row.Scan(&docID)
+						return docID, err
+					})
+				return err
+			}, 0,
+		); err != nil {
+			return fmt.Errorf(
+				"scanning for pending/failed documents failed: %w", err)
+		}
+		if len(docIDs) == 0 {
+			break
+		}
+		// TODO: Load docs, forward them and store the result.
+	}
+	return nil
+}
+
 func (f *forwarder) kill() {
 	f.fns <- func(f *forwarder) { f.done = true }
+}
+
+// ping should be called by the manager to signal the fowarder
+// that there are documents to forward.
+func (f *forwarder) ping() {
+	f.fns <- func(f *forwarder) {}
 }
 
 func (f *forwarder) acceptsPublisher(publisher string) bool {
