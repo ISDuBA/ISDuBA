61346d80d72e4a2482560589bb8a84c4eab0d0c2
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2026-04-16 13:44:38 +0200

    More on creating a text only where clause term.

--- pkg/database/query/textonly.go
+++ pkg/database/query/textonly.go
@@ -8,19 +8,32 @@
 
 package query
 
-import "strings"
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
 
 // CreateTextSearchWhereClause creates an SQL WHERE clause to
 // search matching strings for a given expression tree.
-func CreateTextSearchWhereClause(e *Expr) (string, []any) {
+func CreateTextSearchWhereClause(column string, e *Expr) (string, []any) {
 	var (
 		b            strings.Builder
 		replacements []any
 	)
 
-	var recurse func(*Expr)
+	textIndex := func(text string) (idx int) {
+		replacement := any(LikeEscape(text))
+		if idx = slices.Index(replacements, replacement); idx > -1 {
+			return idx
+		}
+		idx = len(replacements)
+		replacements = append(replacements, replacement)
+		return
+	}
 
-	op := func(curr *Expr, operator string) {
+	var recurse func(*Expr)
+	opTerm := func(curr *Expr, operator string) {
 		b.WriteByte('(')
 		for i, child := range curr.children {
 			if i > 0 {
@@ -32,22 +45,34 @@ func CreateTextSearchWhereClause(e *Expr) (string, []any) {
 		}
 		b.WriteByte(')')
 	}
+	notTerm := func(curr *Expr) {
+		b.WriteString("(NOT(")
+		if len(curr.children) > 0 {
+			recurse(curr.children[0])
+		} else {
+			// XXX: This should not happend.
+			b.WriteString("FALSE")
+		}
+		b.WriteString("))")
+	}
+	searchTerm := func(curr *Expr) {
+		b.WriteByte('(')
+		b.WriteString(column)
+		b.WriteString(" ILIKE $")
+		b.WriteString(strconv.Itoa(textIndex(curr.stringValue)))
+		b.WriteByte(')')
+	}
+	// Flatten the tree.
 	recurse = func(curr *Expr) {
 		switch curr.exprType {
 		case and:
-			op(curr, "AND")
+			opTerm(curr, "AND")
 		case or:
-			op(curr, "OR")
+			opTerm(curr, "OR")
 		case not:
-			b.WriteString("(NOT(")
-			if len(curr.children) > 0 {
-				recurse(curr.children[0])
-			} else {
-				b.WriteString("FALSE")
-			}
-			b.WriteString("))")
-		case ilike:
-			// TODO: Implement me!
+			notTerm(curr)
+		case search:
+			searchTerm(curr)
 		default:
 			// All other conditions are assumed to be fulfilled.
 			b.WriteString("TRUE")
