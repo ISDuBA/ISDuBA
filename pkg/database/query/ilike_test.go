ba2550fd1eb51d4bf8d361e02c6e216f6a3db3b8
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2026-05-26 13:17:59 +0200

    Add one more unit test for ilike search

--- pkg/database/query/ilike_test.go
+++ pkg/database/query/ilike_test.go
@@ -34,3 +34,26 @@ func TestCompileILike(t *testing.T) {
 		t.Errorf("pairs: have: %v expected: %v", havePairs, expectedPairs)
 	}
 }
+
+func TestCarriageReturn(t *testing.T) {
+	const txt = "PC contains a vulnerability that allows hpack table accounting errors could lead to " +
+		"unwanted disconnects between clients and servers in exceptional cases. Three vectors were found " +
+		"that allow the following DOS attacks: - Unbounded memory buffering in the HPACK parser - Unbounded " +
+		"CPU consumption in the HPACK parser The unbounded CPU consumption is down to a copy that occurred " +
+		"per-input-block in the parser, and because that could be unbounded due to the memory copy bug we " +
+		"end up with a parsing loop, with n selected by the client. The unbounded memory buffering bugs: - " +
+		"The header size limit check was behind the string reading code, so we needed to first buffer up to " +
+		"a 4 gigabyte string before rejecting it as longer than 8 or 16kb. - HPACK varints have an encoding " +
+		"quirk whereby an infinite number of 0’s can be added at the start of an integer. gRPC’s hpack " +
+		"parser needed to read all of them before concluding a parse. - gRPC’s metadata overflow check " +
+		"was performed per frame, so that the following sequence of frames could cause infinite buffering: " +
+		"HEADERS: containing a: 1 CONTINUATION: containing a: 2 CONTINUATION: containing a: 3 etc…\r+ " +
+		" - Unbounded memory buffering in the HPACK parser\r"
+	expr := MustCompileILike(`%HEADERS%`)
+	havePairs := expr.Search(txt)
+	for _, pair := range havePairs {
+		if have := txt[pair[0] : pair[0]+pair[1]]; have != "HEADERS" {
+			t.Errorf("pair %+v results in %q not \"HEADERS\"", pair, have)
+		}
+	}
+}
