a3b3c87d6fa8df88b422f6777cb949c2dce24d8f
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2026-09-04 14:17:44 +0200

    Prefill products and ids text tables.

--- pkg/database/migrations/041-fill-product_names_and_ids.sql
+++ pkg/database/migrations/041-fill-product_names_and_ids.sql
@@ -26,10 +26,41 @@ CREATE TABLE products_id_texts (
 CREATE INDEX ON products_id_texts(documents_id);
 CREATE INDEX ON products_id_texts(txt_id);
 
+WITH pn AS (
+  SELECT
+    DISTINCT jsonb_path_query(document, '$.product_tree.**.product.name')::int num,
+    id
+  FROM
+    documents
+),
+data AS (
+  SELECT
+    documents_id,
+    pn.num AS num,
+    txt_id
+  FROM pn JOIN documents_texts dts
+     ON pn.id = dts.documents_id AND pn.num = dts.num
+)
+INSERT INTO products_name_texts
+  SELECT * FROM data;
 
+WITH pid AS (
+  SELECT
+    DISTINCT jsonb_path_query(document, '$.product_tree.**.product.product_id')::int num,
+    id
+  FROM
+    documents
+),
+data AS (
+  SELECT
+    documents_id,
+    pid.num AS num,
+    txt_id
+  FROM pid JOIN documents_texts dts
+     ON pid.id = dts.documents_id AND pid.num = dts.num
+)
+INSERT INTO products_id_texts
+  SELECT * FROM data;
 
 GRANT INSERT, DELETE, SELECT, UPDATE ON products_name_texts TO {{ .User | sanitize }};
 GRANT INSERT, DELETE, SELECT, UPDATE ON products_id_texts   TO {{ .User | sanitize }};
-
-
-
