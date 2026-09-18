12a2113e0de69278341268dc10195fd72b7d5623
Christoph Klassen <christoph.klassen@intevation.de>
2026-09-18 17:22:34 +0200

    Improve mechanism to decide which tabs are detached

--- client/src/lib/Advisories/CSAFWebview/Webview.svelte
+++ client/src/lib/Advisories/CSAFWebview/Webview.svelte
@@ -28,14 +28,15 @@
     getAdvisorySearchHit,
     getProductVulneravbilities
   } from "../advisory.svelte";
-  import type { DocumentReferences } from "../types/csaf-2.1";
   import {
     getAcknowledgments,
     getAliases,
     getInvolvement,
     getNotes,
+    getProductTree,
     getReferences,
-    getRevisionHistory
+    getRevisionHistory,
+    getVulnerabilities
   } from "../docmodel";
   import Involvement from "./vulnerabilities/vulnerability/involvements/Involvement.svelte";
 
@@ -91,6 +92,10 @@
   let aliases = $derived(getAliases(doc));
   let notes = $derived(getNotes(doc));
   let involvement = $derived(getInvolvement(doc));
+  let productTree = $derived(getProductTree(doc));
+  let references = $derived(getReferences(doc));
+  let revisionHistory = $derived(getRevisionHistory(doc));
+  let vulnerabilities = $derived(getVulnerabilities(doc));
 
   // When a link in a component inside the Webview is clicked we want the appropriate
   // tab to be opened.
@@ -121,37 +126,30 @@
     return open;
   });
 
-  let maxTabs: number = $derived.by(() => {
-    return Object.keys(tabOpen).length;
+  // All tabs that should be displayed
+  let availableTabs: string[] = $derived.by(() => {
+    const tabs = [];
+    if (productTree) tabs.push("productTree");
+    if (vulnerabilities) tabs.push("vulnerabilities");
+    if (notes) tabs.push("notes");
+    if (acknowledgments) tabs.push("acknowledgments");
+    if (references) tabs.push("references");
+    if (revisionHistory) tabs.push("revisionHistory");
+    if (involvement) tabs.push("involvement");
+    return tabs;
   });
-  let tabCount: number = $derived.by(() => {
-    let count = 1;
-    if (doc?.product_tree) count++;
-    if (doc?.vulnerabilities) count++;
-    if (notes) count++;
-    if (acknowledgments) count++;
-    if (
-      doc !== null &&
-      getReferences(doc) !== undefined &&
-      (getReferences(doc) as DocumentReferences).length > 0
-    )
-      count++;
-    if (getRevisionHistory(doc)) count++;
-    if (involvement) count++;
-    return count;
+  // Number of tabs that are placed next to the general section
+  let numberOfDetachedTabs: number = $derived(Math.max(0, Math.floor(gridWidth / 750) - 1));
+  let detachedTabs: string[] = $derived.by(() => {
+    return availableTabs.filter((_tab, index) => index < numberOfDetachedTabs);
   });
-  let missingTabs: number = $derived.by(() => {
-    return maxTabs - tabCount;
-  });
-  // Number of sections that can be shown next to each other
-  let screenPhase: number = $derived(Math.max(0, Math.floor(gridWidth / 750 - 2 + missingTabs)));
-  let detachProductTree = $derived(screenPhase > 0);
-  let detachVulnerabilities = $derived(screenPhase > 1);
-  let detachNotes = $derived(screenPhase > 2);
-  let detachAcknowledgment = $derived(screenPhase > 3);
-  let detachReferences = $derived(screenPhase > 4);
-  let detachRevisionHistory = $derived(screenPhase > 5);
-  let detachInvolvement = $derived(screenPhase > 6);
+  let detachProductTree = $derived(detachedTabs.includes("productTree"));
+  let detachVulnerabilities = $derived(detachedTabs.includes("vulnerabilities"));
+  let detachNotes = $derived(detachedTabs.includes("notes"));
+  let detachAcknowledgment = $derived(detachedTabs.includes("acknowledgments"));
+  let detachReferences = $derived(detachedTabs.includes("references"));
+  let detachRevisionHistory = $derived(detachedTabs.includes("revisionHistory"));
+  let detachInvolvement = $derived(detachedTabs.includes("involvement"));
 
   $effect(() => {
     const path = getAdvisorySearchHit()?.path;
@@ -233,7 +231,7 @@
         <ValueList label="Aliases" values={aliases} path="/document/tracking/aliases" />
       </div>
     {/if}
-    {#if screenPhase < Object.keys(tabOpen).length}
+    {#if numberOfDetachedTabs < Object.keys(tabOpen).length}
       <Tabs class="mb-2 flex flex-wrap space-x-2 gap-y-2 rtl:space-x-reverse">
         <TabItem
           activeClass={tabItemActiveClass}
@@ -420,7 +418,7 @@
     </div>
   {/if}
 
-  {#if detachInvolvement}
+  {#if detachInvolvement && involvement}
     <div>
       <FakeButton active>Involvement</FakeButton>
       <div class="mt-2 mb-4 h-px bg-gray-200 dark:bg-gray-700"></div>
